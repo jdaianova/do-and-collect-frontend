67fb8bf0d5ee9789e336479e6b7f0394ac6d8f21
Julia <jdaianova@gmail.com>
2024-05-31 11:24:30 +0200

    change apiBaseUrl

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiBaseUrl: 'http://localhost:4000/api'
+    //apiBaseUrl: 'http://localhost:4000/api'
+    apiBaseUrl: 'https://do-and-collect-backend.onrender.com/api'
   };
   
   export default config;
